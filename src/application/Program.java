4f6921c615c67b788c112b7ee7925742bcdd365b
Felipy <tercetime18@gmail.com>
2025-08-02 15:25:57 -0300

    studies of Set (TreeSet)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,27 @@
 package application;
 
+import java.util.*;
+
 public class Program {
 	public static void main(String[] args) {
+		Set<Integer> numbersInTreeSet = new TreeSet<>();
 		
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("add three numbers: ");
+			for (int i = 0; i < 3; i++) {
+				int number = sc.nextInt();
+				numbersInTreeSet.add(number);
+			}
+			
+			System.out.println("The numbers in ascending order: " + numbersInTreeSet);
+			
+			
+			
+			
+			
+			
+		} catch (Exception e) {
+			System.out.println("An error occurred: " + e.getMessage());
+		}
 	}
 }
